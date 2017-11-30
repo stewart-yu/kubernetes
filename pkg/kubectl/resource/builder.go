ad1031ae764828e8387d1863cef6396493f63aad
stewart-yu <yuchuanjian@huawei.com>
2017-11-30 16:40:51 +0800

    fix

--- pkg/kubectl/resource/builder.go
+++ pkg/kubectl/resource/builder.go
@@ -985,9 +985,9 @@ func HasNames(args []string) (bool, error) {
 
 // MultipleTypesRequested returns true if the provided args contain multiple resource kinds
 func MultipleTypesRequested(args []string) bool {
-	if len(args) == 1 && args[0] == "all" {
+	/*if len(args) == 1 && args[0] == "all" {
 		return true
-	}
+	}*/
 
 	args = normalizeMultipleResourcesArgs(args)
 	rKinds := sets.NewString()
