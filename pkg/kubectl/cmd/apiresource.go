d832e19c41701faa0a9aacd56ba40a256d723190
stewart-yu <yuchuanjian@huawei.com>
2018-01-08 21:13:50 +0800

    fix

--- pkg/kubectl/cmd/apiresource.go
+++ pkg/kubectl/cmd/apiresource.go
@@ -17,12 +17,21 @@ limitations under the License.
 package cmd
 
 import (
+	"encoding/json"
 	"github.com/spf13/cobra"
 	"k8s.io/kubernetes/pkg/kubectl/util/i18n"
+	"errors"
 	"fmt"
 	"io"
+	"strings"
+	"github.com/ghodss/yaml"
 	"k8s.io/kubernetes/pkg/kubectl/cmd/templates"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	cmdutil "k8s.io/kubernetes/pkg/kubectl/cmd/util"
+	"github.com/golang/glog"
+	"k8s.io/kubernetes/pkg/printers"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"sort"
 )
 
 var (
@@ -30,6 +39,12 @@ var (
 		# Print the supported API resource
 		kubectl apiresource`))
 )
+
+// groupResource contains the APIGroup and APIResource
+type groupResource struct {
+	APIGroup    string
+	APIResource metav1.APIResource
+}
 
 // apiresourcesOptions: describe the options available to users of the "kubectl
 // apiresources" command.
@@ -75,7 +90,7 @@ func (o *ApiResourcesOptions) Complete(cmd *cobra.Command) error {
 
 func (o *ApiResourcesOptions) ValidateArgs(cmd *cobra.Command, args []string) error {
 	if len(args) > 1 {
-		return fmt.Errorf("not expect too many arguments")
+		return fmt.Errorf("Invalid number of arguments: expected at most 1")
 	}
 
 	outputMode := cmdutil.GetFlagString(cmd, "output")
@@ -86,7 +101,134 @@ func (o *ApiResourcesOptions) ValidateArgs(cmd *cobra.Command, args []string) er
 	return nil
 }
 
+func (o *ApiResourcesOptions) GetApiResource(f cmdutil.Factory) ([]groupResource, error) {
+	discoveryClient, err := f.DiscoveryClient()
+	if err != nil {
+		return nil, err
+	}
+
+	// Always request fresh data from the server
+	discoveryClient.Invalidate()
+
+	lists, err := discoveryClient.ServerPreferredResources()
+	if err != nil {
+		return nil, fmt.Errorf("get available api resources from server failed: %v", err)
+	}
+
+	resources := []groupResource{}
+
+	for _, list := range lists {
+		if len(list.APIResources) == 0 {
+			continue
+		}
+		gv, err := schema.ParseGroupVersion(list.GroupVersion)
+		if err != nil {
+			glog.V(1).Infof("Unable to parse groupversion %s:%s", list.GroupVersion, err.Error())
+			continue
+		}
+		for _, resource := range list.APIResources {
+			if len(resource.Verbs) == 0 {
+				continue
+			}
+			//// filter apiGroup
+			//if o.apiGroup != gv.Group {
+			//	continue
+			//}
+			//// filter namespaced
+			//if o.namespaced != resource.Namespaced {
+			//	continue
+			//}
+			resources = append(resources, groupResource{
+				APIGroup:    gv.Group,
+				APIResource: resource,
+			})
+		}
+	}
+
+	return resources, nil
+}
+
 func (o *ApiResourcesOptions) RunApiResources(cmd *cobra.Command, f cmdutil.Factory) error {
 
+	w := printers.GetNewTabWriter(o.out)
+	defer w.Flush()
+
+	resources, err := o.GetApiResource(f)
+	if err != nil {
+		glog.V(1).Infof("Get available cluster resources failed: %v", err)
+	}
+
+	sort.Stable(sortableGroupResource(resources))
+
+	var resultResources string
+	// print output
+	switch o.output {
+	case "":
+		resultResources = "NAME\tSHORTNAMES\tAPIGROUP\tNAMESPACED\tKIND\n"
+		for _, r := range resources {
+			resultResources = fmt.Sprintf("%s%s\t%s\t%s\t%v\t%s\n",
+				resultResources,
+				r.APIResource.Name,
+				strings.Join(r.APIResource.ShortNames, ","),
+				r.APIGroup,
+				r.APIResource.Namespaced,
+				r.APIResource.Kind)
+		}
+		// normal
+	case "wide":
+		resultResources = "NAME\tSHORTNAMES\tAPIGROUP\tNAMESPACED\tKIND\tVERBS\n"
+		for _, r := range resources {
+			resultResources = fmt.Sprintf("%s%s\t%s\t%s\t%v\t%s\t%v\n",
+				resultResources,
+				r.APIResource.Name,
+				strings.Join(r.APIResource.ShortNames, ","),
+				r.APIGroup,
+				r.APIResource.Namespaced,
+				r.APIResource.Kind,
+				r.APIResource.Verbs)
+		}
+	case "yaml":
+		// output with yaml
+		for _, r := range resources {
+			marshalled, err := yaml.Marshal(&r.APIResource)
+			if err != nil {
+				return err
+			}
+			resultResources = fmt.Sprintf("%s%s\n", resultResources, string(marshalled))
+		}
+	case "json":
+		// output with json
+		for _, r := range resources {
+			marshalled, err := json.MarshalIndent(&r.APIResource, "", "  ")
+			if err != nil {
+				return err
+			}
+			resultResources = fmt.Sprintf("%s%s\n", string(marshalled))
+		}
+	default:
+		// There is a bug in the program if we hit this case.
+		// However, we follow a policy of never panicking.
+		return fmt.Errorf("versionOptions were not validated: --output=%q should have been rejected", o.output)
+	}
+	if len(resources) > 0{
+		fmt.Fprintf(w, "%v", resultResources)
+	}else{
+		fmt.Fprintln(w, "No resources found.")
+	}
+
 	return nil
+}
+
+type sortableGroupResource []groupResource
+
+func (s sortableGroupResource) Len() int      { return len(s) }
+func (s sortableGroupResource) Swap(i, j int) { s[i], s[j] = s[j], s[i] }
+func (s sortableGroupResource) Less(i, j int) bool {
+	ret := strings.Compare(s[i].APIGroup, s[j].APIGroup)
+	if ret > 0 {
+		return false
+	} else if ret == 0 {
+		return strings.Compare(s[i].APIResource.Name, s[j].APIResource.Name) < 0
+	}
+	return true
 }
